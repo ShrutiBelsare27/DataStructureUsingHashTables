de2b907e2adf1bafaff16b67bf1f50a1528ed64d
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2021-11-09 00:01:58 +0530

    Added: data structure using hash table

--- DataStructureUsingHashTables/DataStructureUsingHashTables/Program.cs
+++ DataStructureUsingHashTables/DataStructureUsingHashTables/Program.cs
@@ -6,7 +6,20 @@ namespace DataStructureUsingHashTables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome message");
+
+            Console.WriteLine("UC1: ");
+            UC1WordFrequency freq = new UC1WordFrequency();
+            freq.Frequency();
+
+            Console.WriteLine("\n UC2: ");
+            UC2FreqWordAndPara freq1 = new UC2FreqWordAndPara();
+            freq1.FreqPara();
+
+
+            Console.WriteLine("\n UC3: ");
+            UC3Remove rm = new UC3Remove();
+            rm.Remove();
+           
         }
     }
 }
---
+++ DataStructureUsingHashTables/DataStructureUsingHashTables/UC2FreqWordAndPara.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructureUsingHashTables
+{
+   public class UC2FreqWordAndPara
+    {
+        public void FreqPara()
+        {
+            MyMapNode<string, int> myMapNode = new MyMapNode<string, int>(5);
+       
+           string sentence = "Paranoids are not paranoid because they are paranoid but because" +
+           " they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] words = sentence.ToLower().Split(" ");
+            foreach (string word in words)
+            {
+                int value = myMapNode.Get(word);
+                if (value == 0)
+                    value = 1;
+                else
+                    value = value + 1;
+                myMapNode.Add(word, value);
+            }
+            int frequency = myMapNode.Get("paranoid");
+            Console.WriteLine("Frequency of word paranoid in paragraph = " + frequency);
+        }
+    }
+}
